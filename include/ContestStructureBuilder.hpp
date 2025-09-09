a7372336e64eaca850608ee6b1d1b06fd42cd00e
Andriy <shostak303@gmail.com>
2025-09-09 17:29:09 +0300

    refactor(builder): move class to separate file

---
+++ include/ContestStructureBuilder.hpp
@@ -0,0 +1,67 @@
+﻿#pragma once
+
+#include <filesystem>
+#include <string>
+
+class ContestStructureBuilder {
+
+private:
+	std::string contestName_;
+	char maxProblemLetter_;
+	std::filesystem::path targetDirectory_;
+
+	/**
+	 * @brief Повертає повний шлях до контесту.
+	 * @returns Повний шлях до контесту.
+	 */
+	[[nodiscard]] std::filesystem::path getContestPath() const;
+
+	/**
+	* @brief Повертає шлях до конкретної задачі
+	* @param problemLetter Літера задачі.
+	* @return Повний шлях до директорії задачі.
+	*/
+	[[nodiscard]] std::filesystem::path getProblemPath(char problemLetter) const;
+
+	/**
+	* @brief Створює головну директорію контесту.
+	*/
+	void createContestDirectory() const;
+
+	/**
+	* @brief Генерує основні файли проекту (README.md, CMakeLists.txt, тощо).
+	*/
+	void generateProjectFiles() const;
+
+	/**
+	* @brief Створює директорії для всіх задач (A, B, C, ...)
+	*/
+	void createProblemDirectories() const;
+
+	/**
+	* @brief Генерує вихідні дані для всіх задач.
+	*/
+	void generateSourceFiles() const;
+
+
+	/**
+	* @brief Створює сурс файли для конкретної задачі.
+	* @param problemLetter Літера задачі
+	*/
+	void generateSourceFilesForProblem(char problemLetter) const;
+
+public:
+	/**
+	 * @brief Конструктор класу ContestStructureBuilder.
+	 * @param contestName Назва контесту
+	 * @param maxProblemLetter Остання літера задачі (A, B, C, ...)
+	 * @param targetDirectory Цільова директорія для створення
+	 */
+	ContestStructureBuilder(std::string contestName, char maxProblemLetter, std::filesystem::path targetDirectory);
+
+	/**
+	* @brief Створює повну архітектуру контесту.
+	* @throws runtime_error Якщо виникла помилка при створенні.
+	*/
+	void build() const;
+};
---
+++ src/ContestStructureBuilder.cpp
@@ -0,0 +1,68 @@
+#include "ContestStructureBuilder.hpp"
+#include <FileSystemManager.hpp>
+#include <TemplateManager.hpp>
+
+#include <string>
+#include <filesystem>
+#include <utility>
+
+ContestStructureBuilder::ContestStructureBuilder(std::string contestName, char maxProblemLetter, std::filesystem::path targetDirectory)
+    : contestName_(std::move(contestName)),
+    maxProblemLetter_(maxProblemLetter),
+    targetDirectory_(std::move(targetDirectory)) {
+}
+
+std::filesystem::path ContestStructureBuilder::getContestPath() const {
+    return targetDirectory_ / contestName_;
+}
+
+std::filesystem::path ContestStructureBuilder::getProblemPath(char problemLetter) const {
+    return getContestPath() / std::string{ problemLetter };
+}
+
+void ContestStructureBuilder::createContestDirectory() const {
+    FileSystemManager::createDirectory(getContestPath());
+}
+
+void ContestStructureBuilder::generateProjectFiles() const {
+    FileSystemManager::createFile(
+        getContestPath() / "CMakeLists.txt",
+        TemplateManager::getCMakeTemplate(contestName_, maxProblemLetter_)
+    );
+    FileSystemManager::createFile(
+        getContestPath() / "README.md",
+        TemplateManager::getReadmeTemplate(contestName_)
+    );
+}
+
+void ContestStructureBuilder::createProblemDirectories() const {
+    for (char problemLetter = 'A'; problemLetter <= maxProblemLetter_; ++problemLetter) {
+        FileSystemManager::createDirectory(getProblemPath(problemLetter));
+    }
+}
+
+void ContestStructureBuilder::generateSourceFiles() const {
+    for (char problemLetter = 'A'; problemLetter <= maxProblemLetter_; ++problemLetter) {
+        generateSourceFilesForProblem(problemLetter);
+    }
+}
+
+void ContestStructureBuilder::generateSourceFilesForProblem(char problemLetter) const {
+    const std::filesystem::path problemDir = getProblemPath(problemLetter);
+    const std::string fileName{ problemLetter };
+    FileSystemManager::createFile(
+        problemDir / (fileName + ".cpp"),
+        TemplateManager::getCppTemplate()
+    );
+    FileSystemManager::createFile(
+        problemDir / (fileName + ".py"),
+        TemplateManager::getPythonTemplate()
+    );
+}
+
+void ContestStructureBuilder::build() const {
+    createContestDirectory();
+    generateProjectFiles();
+    createProblemDirectories();
+    generateSourceFiles();
+}
