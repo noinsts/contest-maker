0f2bd37f379edac95f4f0a1e91fbca8c6bc50f83
Andriy <shostak303@gmail.com>
2025-09-10 00:07:35 +0300

    refactor(templates): add docstrings

--- include/core/TemplateManager.hpp
+++ include/core/TemplateManager.hpp
@@ -1,11 +1,31 @@
-#pragma once
+﻿#pragma once
 #include <string>
 
 class TemplateManager {
-    public: 
-        static std::string getCppTemplate();
-        static std::string getJavaTemplate();
-        static std::string getPythonTemplate();
-        static std::string getReadmeTemplate(const std::string& projectName);
-        static std::string getCMakeTemplate(const std::string& projectName, const char& maxSubDirs);
+
+public: 
+    /**
+    * @brief Шаблон для С++.
+    */
+    static std::string getCppTemplate();
+
+    /**
+    * @brief Шаблон для Java.
+    */
+    static std::string getJavaTemplate();
+
+    /**
+    * @brief Шаблон для Python.
+    */
+    static std::string getPythonTemplate();
+
+    /**
+    * @brief Шаблон для README.
+    */
+    static std::string getReadmeTemplate(const std::string& projectName);
+
+    /**
+    * @brief Шаблон для CMakeLists.
+    */
+    static std::string getCMakeTemplate(const std::string& projectName, const char& maxSubDirs);
 };
